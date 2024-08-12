ff36d91e873b2fb04c197adc040de4bed19a01d5
root <root@Venku-PC>
2024-08-12 22:32:15 +0530

    added java file

---
+++ f.java
@@ -0,0 +1,14 @@
+public class LoginUser
+{
+        public static void main(String args[])
+        {
+                int i;
+
+                for(i=0;i<=45;i++){
+
+                
+
+                        System.out.println("Number: " + i);
+                }
+        }
+}
